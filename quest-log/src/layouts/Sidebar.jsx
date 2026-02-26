5c8e109b777a66c978218fd8b3dfb0bbd8fc1dc8
Izel Ozhan <izelozhan@gmail.com>
2026-02-26 12:51:01 -0600

    create Sidebar navigation menu

--- quest-log/src/layouts/Sidebar.jsx
+++ quest-log/src/layouts/Sidebar.jsx
@@ -6,25 +6,56 @@ import dashboard from "../images/dashboard.svg";
 import Dashboard from "../pages/Dashboard";
 import Habits from "../pages/Habits";
 import Stats from "../pages/Stats";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function Sidebar() {
+  const location = useLocation();
+
+  const [closeMenu, setCloseMenu] = useState(false);
+
+  const handleCloseMenu = () => {
+    setCloseMenu(!closeMenu);
+  };
+
   return (
-    <div className="sidebar">
-      <div className="logoContainer">
+    <div className={closeMenu === false ? "sidebar" : "sidebar active"}>
+      <div
+        className={
+          closeMenu === false ? "logoContainer" : "logoContainer active"
+        }
+      >
         <h2 className="title">Quest Log.</h2>
       </div>
-      <div className="burgerContainer">
-        <div className="burgerTrigger"></div>
+      <div
+        className={
+          closeMenu === false ? "burgerContainer" : "burgerContainer active"
+        }
+      >
+        <div
+          className="burgerTrigger"
+          onClick={() => {
+            handleCloseMenu();
+          }}
+        ></div>
         <div className="burgerMenu"></div>
       </div>
-      <div className="profileContainer">
+      <div
+        className={
+          closeMenu === false ? "profileContainer" : "profileContainer active"
+        }
+      >
         <img src={profile} alt="profilePicture" className="profile" />
         <div className="profileContents">
           <p className="name">Hello, Izzy!</p>
           <p className="email">something@gmail.com</p>
         </div>
       </div>
-      <div className="contentsContainer">
+      <div
+        className={
+          closeMenu === false ? "contentsContainer" : "contentsContainer active"
+        }
+      >
         <ul>
           <li>
             <img src={dashboard} alt="dashboard" />
@@ -40,7 +71,7 @@ function Sidebar() {
           </li>
           <li>
             <img src={settings} alt="settings" />
-            <a href="/settings">stats</a>
+            <a href="/settings">settings</a>
           </li>
         </ul>
       </div>
